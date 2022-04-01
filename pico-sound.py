3eab057d50248a7145952f964d609e9b3432cc5a
joe <jwennesheimer@gmail.com>
2022-03-31 20:33:40 -0500

    file now picks random scream to play

--- pico-sound.py
+++ pico-sound.py
@@ -1,6 +1,7 @@
 import time
 import board
 import digitalio
+import random
 
 from audiomp3 import MP3Decoder
 
@@ -16,7 +17,11 @@ button1 = digitalio.DigitalInOut(board.GP20)
 button1.switch_to_input(pull=digitalio.Pull.UP)
 
 # Replace with your mp3files name
-mp3files = ["start-the-day-with-3lber.mp3"]
+mp3files = ["start-the-day-with-3lber.mp3", "10lber-bro.mp3",
+            "did-that-just-happen.mp3", "game-changer.mp3",
+            "lets-go.mp3", "never-give-up.mp3", "start-the-day-with-3lber.mp3"]
+
+
 
 # You have to specify some mp3 file when creating the decoder
 mp3 = open(mp3files[0], "rb")
@@ -25,9 +30,10 @@ audio = AudioOut(board.GP18)
 
 while True:
     if not button1.value:
-        decoder.file = open(mp3files[0], "rb")
+        file = random.randint(0,6)
+        decoder.file = open(mp3files[file], "rb")
         audio.play(decoder)
-        print("playing", mp3files[0])
+        print("playing", mp3files[file])
 
         t = time.monotonic()
         while time.monotonic() - t < 6:
